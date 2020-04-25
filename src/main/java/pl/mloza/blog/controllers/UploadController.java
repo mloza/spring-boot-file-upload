32609b890d770450c3f66e848581a8e10b2a0bc3
michalloza <michal.loza@codewise.com>
2020-04-25 19:35:44 +0200

    Add code to get image from server

--- src/main/java/pl/mloza/blog/controllers/UploadController.java
+++ src/main/java/pl/mloza/blog/controllers/UploadController.java
@@ -1,13 +1,15 @@
 package pl.mloza.blog.controllers;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.net.URLConnection;
+import java.nio.file.Files;
 
 @Controller
 public class UploadController {
@@ -33,5 +35,17 @@ public class UploadController {
         }
 
         return "ok!";
+    }
+
+    @GetMapping("image/{name}")
+    public ResponseEntity showImage(@PathVariable String name) throws IOException {
+        File file = new File("uploads/" + name);
+        if (!file.exists()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(URLConnection.guessContentTypeFromName(name)))
+                .body(Files.readAllBytes(file.toPath()));
     }
 }
